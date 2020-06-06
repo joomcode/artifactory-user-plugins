86f16d1b759917d17c55d1b23744d5cd33341090
Artem V. Navrotskiy <bozaro@yandex.ru>
2020-06-07 01:17:10 +0300

    INFRA-3063: Add `group` for docker image

--- cleanup/cleanDockerImages/CleanDockerImagesTest.groovy
+++ cleanup/cleanDockerImages/CleanDockerImagesTest.groovy
@@ -90,11 +90,100 @@ class CleanDockerImagesTest extends Specification {
         repo.delete()
     }
 
-    void mkImage(repo, path, content, ct) {
-        def prop = 'docker.label.com.joom.retention.maxCount'
+    def 'group clean docker images plugin test'() {
+        setup:
+        def baseurl = 'http://localhost:8088/artifactory'
+        def artifactory = ArtifactoryClientBuilder.create().setUrl(baseurl)
+                .setUsername('admin').setPassword('password').build()
+
+        def builder = artifactory.repositories().builders()
+        def local = builder.localRepositoryBuilder().key('example-docker-local')
+                .repositorySettings(new DockerRepositorySettingsImpl()).build()
+        artifactory.repositories().create(0, local)
+        def repo = artifactory.repository('example-docker-local')
+        mkImage(repo, 'foo1/bar/manifest.json', 'text1', 1, 'bar')
+        mkImage(repo, 'foo1/baz/manifest.json', 'text2', 1, 'baz')
+        mkImage(repo, 'foo1/ban/manifest.json', 'text3', 1, 'ban')
+        mkImage(repo, 'foo2/bar/manifest.json', 'text4', 1, 'bar')
+        mkImage(repo, 'foo2/baz/manifest.json', 'text5', 1, 'baz')
+        mkImage(repo, 'foo2/ban/manifest.json', 'text6', 1, 'ban')
+        mkImage(repo, 'foo3/bar/manifest.json', 'text7', 1, 'bar')
+        mkImage(repo, 'foo3/ban/manifest.json', 'text8', 1, 'ban')
+        mkImage(repo, 'foo3/baz/manifest.json', 'text9', 1, 'baz')
+
+        when:
+        def resp = artifactory.plugins().execute('cleanDockerImages').sync()
+
+        then:
+        new JsonSlurper().parseText(resp).status == 'okay'
+
+        when:
+        repo.file('foo1/bar/manifest.json').info()
+
+        then:
+        thrown(HttpResponseException)
+
+        when:
+        repo.file('foo1/baz/manifest.json').info()
+
+        then:
+        notThrown(HttpResponseException)
+
+        when:
+        repo.file('foo1/ban/manifest.json').info()
+
+        then:
+        notThrown(HttpResponseException)
+
+        when:
+        repo.file('foo2/bar/manifest.json').info()
+
+        then:
+        thrown(HttpResponseException)
+
+        when:
+        repo.file('foo2/baz/manifest.json').info()
+
+        then:
+        notThrown(HttpResponseException)
+
+        when:
+        repo.file('foo2/ban/manifest.json').info()
+
+        then:
+        notThrown(HttpResponseException)
+
+        when:
+        repo.file('foo3/bar/manifest.json').info()
+
+        then:
+        thrown(HttpResponseException)
+
+        when:
+        repo.file('foo3/baz/manifest.json').info()
+
+        then:
+        notThrown(HttpResponseException)
+
+        when:
+        repo.file('foo3/ban/manifest.json').info()
+
+        then:
+        notThrown(HttpResponseException)
+
+        cleanup:
+        repo.delete()
+    }
+
+    void mkImage(repo, path, content, ct, String group = null) {
+        def countProp = 'docker.label.com.joom.retention.maxCount'
+        def groupProp = 'docker.label.com.joom.retention.group'
         def stream = new ByteArrayInputStream(content.getBytes('utf-8'))
         repo.upload(path, stream).doUpload()
-        repo.file(path).properties().addProperty(prop, "$ct").doSet()
+        repo.file(path).properties().addProperty(countProp, "$ct").doSet()
+        if (group != null) {
+            repo.file(path).properties().addProperty(groupProp, group).doSet()
+        }
         sleep(100)
     }
 }
--- cleanup/cleanDockerImages/cleanDockerImages.groovy
+++ cleanup/cleanDockerImages/cleanDockerImages.groovy
@@ -29,6 +29,82 @@ import java.util.concurrent.TimeUnit
 
 Repositories repositories
 
+class GroupInfo {
+    RepoPath keep
+    List<RepoPath> images
+}
+
+class ImageGroups {
+    Map<String, GroupInfo> groups = new HashMap<>()
+    List<String> deleted = new ArrayList<>()
+    Repositories repositories
+    def log
+    boolean dryRun
+
+    boolean isProtected(String group) {
+        if (group == null) {
+            return false
+        }
+        GroupInfo info = groups.get(group)
+        if (info == null) {
+            return false
+        }
+        return info.keep
+    }
+
+    void protectGroup(String group, RepoPath path) {
+        GroupInfo info = groups.get(group)
+        if (info == null) {
+            info = new GroupInfo()
+            groups.put(group, info)
+        }
+        if (info.keep == null) {
+            info.keep = path
+            info.images = null
+        }
+    }
+
+    void removeImage(ImageInfo image) {
+        String group = image.getGroup(repositories)
+        if (group == null) {
+            deleteImage(image.repo.repoPath)
+            return
+        }
+        GroupInfo info = groups.get(group)
+        if (info == null) {
+            info = new GroupInfo()
+            groups.put(group, info)
+        }
+        if (info.keep != null) {
+            log.debug("Keep image: ${image.repo.repoPath} - image in same group (${info.keep}) is protected")
+            return
+        }
+        if (info.images == null) {
+            info.images = new ArrayList<>()
+        }
+        info.images.add(image.repo.repoPath)
+    }
+
+    void cleanup() {
+        for (info in groups.values()) {
+            if (info.images == null) {
+                continue
+            }
+            for (image in info.images) {
+                deleteImage(image)
+            }
+        }
+    }
+
+    private void deleteImage(RepoPath path) {
+        log.warn("Removing image: ${path}")
+        deleted << path.id
+        if (!dryRun) {
+            repositories.delete(path)
+        }
+    }
+}
+
 class ImageInfo {
     ItemInfo manifest
     ItemInfo repo
@@ -64,6 +140,10 @@ class ImageInfo {
     Integer getMaxDays(Repositories repositories) {
         String maxDaysProp = repositories.getProperty(manifest.repoPath, "docker.label.com.joom.retention.maxDays")
         return maxDaysProp ? maxDaysProp.toInteger() : null
+    }
+
+    String getGroup(Repositories repositories) {
+        return repositories.getProperty(manifest.repoPath, "docker.label.com.joom.retention.group")
     }
 
     boolean isPullProtected(Repositories repositories) {
@@ -112,7 +192,7 @@ executions {
         def defaultMaxDays = parsed.defaultMaxDays
         def dryRun = params['dryRun'] ? params['dryRun'][0] as boolean : false
         repos.each {
-            log.debug("Cleaning Docker images in repo: $it")
+            log.error("Cleaning Docker images in repo: $it")
             def del = buildParentRepoPaths(RepoPathFactory.create(it), defaultMaxDays, dryRun)
             deleted.addAll(del)
         }
@@ -123,7 +203,14 @@ executions {
 }
 
 def buildParentRepoPaths(path, int defaultMaxDays, boolean dryRun) {
-    return registryTraverse(repositories.getItemInfo(path), defaultMaxDays, dryRun)
+    ImageGroups groups = new ImageGroups(
+            repositories: repositories,
+            log: log,
+            dryRun: dryRun,
+    )
+    registryTraverse(repositories.getItemInfo(path), groups, defaultMaxDays)
+    groups.cleanup()
+    return groups.deleted
 }
 
 // Traverse through the docker repo (directories and sub-directories) and:
@@ -136,8 +223,8 @@ def buildParentRepoPaths(path, int defaultMaxDays, boolean dryRun) {
 // - keep recently pulled images with `com.joom.retention.pullProtectDays` label (-1 - infinity time);
 // - remove docker images if `com.joom.retention.maxDays` policy applices (-1 - infinity time);
 // - aggregate the images for `com.joom.retention.maxCount` policy.
-List<String> registryTraverse(ItemInfo parentInfo, int defaultMaxDays, boolean dryRun, Map<String, List<ImageInfo>> parentGroups = null, List<String> deleted = []) {
-    List<RepoPath> removeSet = new ArrayList<>()
+def registryTraverse(ItemInfo parentInfo, ImageGroups groups, int defaultMaxDays, Map<String, List<ImageInfo>> parentGroups = null, List<String> deleted = []) {
+    List<ImageInfo> removeSet = new ArrayList<>()
     List<ItemInfo> pendingQueue = new ArrayList<>()
 
     // Process current image
@@ -164,18 +251,30 @@ List<String> registryTraverse(ItemInfo parentInfo, int defaultMaxDays, boolean d
     // Process registry and subregistry
     Map<String, List<ImageInfo>> childGroups = new HashMap<>()
     for (item in pendingQueue) {
-        registryTraverse(item, defaultMaxDays, dryRun, childGroups, deleted)
+        registryTraverse(item, groups, defaultMaxDays, childGroups, deleted)
     }
 
     try {
         while (manifestInfo != null) {
-            log.debug("Scanning File: $manifestInfo.repoPath")
+            log.error("Scanning File: $manifestInfo.repoPath")
             ImageInfo info = getImageInfo(parentInfo, manifestInfo)
             if (info == null) {
                 log.error("Invalid image labels: $parentRepoPath")
                 break
             }
             info.layerPaths = layerPaths
+
+            String group = info.getGroup(repositories)
+            if (groups.isProtected(group)) {
+                log.error("Keep image: $parentRepoPath - image in same group is protected")
+
+                String maxCountGroup = info.getMaxCountGroup(repositories)
+                if (!parentGroups.containsKey(maxCountGroup)) {
+                    parentGroups.put(maxCountGroup, new ArrayList<>())
+                }
+                parentGroups[maxCountGroup] << info
+                break
+            }
 
             long expireDate = info.getExpireDate(repositories)
             if (expireDate > 0) {
@@ -184,7 +283,8 @@ List<String> registryTraverse(ItemInfo parentInfo, int defaultMaxDays, boolean d
                     removeSet << parentRepoPath
                     break
                 }
-                log.debug("Keep image: $parentRepoPath - expireDate is in future")
+                log.error("Keep image: $parentRepoPath - expireDate is in future")
+                groups.protectGroup(group, info.repo.repoPath)
                 break
             }
 
@@ -196,17 +296,19 @@ List<String> registryTraverse(ItemInfo parentInfo, int defaultMaxDays, boolean d
 
             boolean maxDaysExpired = (maxDays != null) && (info.getCreatedTime() + maxDays * oneDay <= now)
             if ((!maxDaysExpired) && (maxCount == null)) {
-                log.debug("Keep image: $parentRepoPath - created recently without count limit")
+                log.error("Keep image: $parentRepoPath - created recently without count limit")
+                groups.protectGroup(group, info.repo.repoPath)
                 break
             }
 
             if (maxDaysExpired) {
                 if (info.isPullProtected(repositories)) {
-                    log.debug("Keep image: $parentRepoPath - created too long, but pulled recently")
+                    log.error("Keep image: $parentRepoPath - created too long, but pulled recently")
+                    groups.protectGroup(group, info.repo.repoPath)
                     break
                 }
-                log.warn("Removing image: $parentRepoPath - created too long")
-                removeSet << parentRepoPath
+                log.error("Obsolete image: $parentRepoPath - created too long")
+                removeSet << info
                 break
             }
 
@@ -218,7 +320,8 @@ List<String> registryTraverse(ItemInfo parentInfo, int defaultMaxDays, boolean d
                 parentGroups[maxCountGroup] << info
                 break
             }
-            log.debug("Keep image: $parentRepoPath - created recently")
+            log.error("Keep image: $parentRepoPath - created recently")
+            groups.protectGroup(group, item)
             break
         }
 
@@ -228,30 +331,23 @@ List<String> registryTraverse(ItemInfo parentInfo, int defaultMaxDays, boolean d
             int keeped = 0
             for (ImageInfo item in group) {
                 if (keeped < item.getMaxCount(repositories)) {
-                    log.debug("Keep image: ${item.repo.repoPath} - max count not reached")
+                    log.error("Keep image: ${item.repo.repoPath} - max count not reached")
+                    groups.protectGroup(item.getGroup(repositories), item.repo.repoPath)
                     keeped++
                     continue
                 }
-                if (item.isPullProtected(repositories)) {
-                    log.debug("Keep image: ${item.repo.repoPath} - max count not reached, but pulled recently")
-                    keeped++
-                    continue
-                }
-                log.warn("Removing image: ${item.repo.repoPath} - too many images")
-                removeSet << item.repo.repoPath
+                log.error("Obsolete image: ${item.repo.repoPath} - too many images")
+                removeSet << item
             }
         }
 
         // Remove images from repository
-        for (RepoPath item in removeSet) {
-            deleted << item.id
-            if (!dryRun) repositories.delete(item)
+        for (ImageInfo item in removeSet) {
+            groups.removeImage(item)
         }
     } catch (IllegalArgumentException e) {
         log.error(e.printStackTrace())
-        return null
     }
-    return deleted
 }
 
 ImageInfo getImageInfo(ItemInfo repo, ItemInfo item) {
